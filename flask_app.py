40cd68d30c8a20f4602a17654f178699ec5ba37f
lkelly00 <lkelly00@gmail.com>
2017-03-10 20:16:12 +0000

    third commit

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import Form, StringField, DecimalField, TextAreaField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.secret_key = 'happypuppy73'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/lkelly00/sites/etsydemo/tmp/database.db'
+db = SQLAlchemy(app)
+
+class Listing(db.Model):
+    __tablename__ = "course"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float(2))
+
+class ListingForm(Form):
+    name = StringField('Name', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    price = DecimalField('Price', validators=[DataRequired()])
 
 @app.route('/')
 def index():
-    return render_template ('index.html')
+    results = Listing.query.filter(1==1).all()
+    return render_template ('index.html', listings=results)
+
+@app.route('/listing/new', methods=['GET','POST'])
+def newlisting():
+    form = ListingForm(request.form)
+    if request.method == 'POST':
+        if form.validate() == False:
+          flash('All fields are required.')
+          return render_template('newlisting.html', form=form)
+        else:
+          lst = Listing(name=form.name.data, description= form.description.data, price=form.price.data )
+          db.session.add(lst)
+          db.session.commit()
+          results = Listing.query.filter(1==1).all()
+          return render_template('index.html', listings=results)
+    else:
+        return render_template ('newlisting.html', form=form)
 
 @app.route('/about')
 def about():
