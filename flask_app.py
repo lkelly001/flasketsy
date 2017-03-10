7759659a0350e2ab85d46d18e8d9a6e96f808841
lkelly00 <lkelly00@gmail.com>
2017-03-10 21:06:05 +0000

    4th commit

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, abort
 from flask_sqlalchemy import SQLAlchemy
 from wtforms import Form, StringField, DecimalField, TextAreaField
 from wtforms.validators import DataRequired
@@ -9,7 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/lkelly00/sites/etsydemo
 db = SQLAlchemy(app)
 
 class Listing(db.Model):
-    __tablename__ = "course"
+    __tablename__ = "listing"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
     description = db.Column(db.String(200))
@@ -40,6 +40,39 @@ def newlisting():
           return render_template('index.html', listings=results)
     else:
         return render_template ('newlisting.html', form=form)
+
+
+@app.route('/listing/show/<listing_id>')
+def listing_show(listing_id):
+    try:
+        lst = Listing.query.get(listing_id)
+    except:
+        abort(404)
+
+    return render_template('listing_show.html', list_id=lst.id, listing=lst)
+
+@app.route('/listing/edit/<listing_id>', methods=['GET','POST'])
+def listing_edit(listing_id):
+    form = ListingForm(request.form)
+    if request.method == 'POST':
+        if form.validate() == False:
+            flash('All fields are required.')
+            return render_template('listing_edit.html', form=form)
+        else:
+            lst = Listing.query.get(listing_id)
+            lst.name=form.name.data
+            lst.description= form.description.data
+            lst.price=form.price.data
+            db.session.commit()
+            flash('Your edits were saved.')
+            return render_template('listing_edit.html', form=form)
+    else:
+        try:
+            lst = Listing.query.get(listing_id)
+            lstForm = ListingForm(obj=lst)
+        except:
+            abort(404)
+        return render_template('listing_edit.html', form=lstForm)
 
 @app.route('/about')
 def about():
